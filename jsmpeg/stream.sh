0c927277e37aeb0630c425f25a7264f6416a97bd
eldiablo100000 <doctor.bearer@gmail.com>
2018-05-02 08:15:22 +0000

    script

---
+++ jsmpeg/stream.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+node websocket-relay.js prova 8081 8082 &
+http-server &
+ffmpeg -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 http://localhost:8081/prova 
+
